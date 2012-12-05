0b811b2ed22c8a1cfda150e4b9e0f4d84ab7e03d
John Smith <garbledtutors@mail.com>
2012-12-04 16:44:52 -0800

    added previous notion data and created update script

---
+++ notion/cfg_defaults.lua
@@ -0,0 +1,16 @@
+--
+-- Notion default settings
+--
+
+dopath("cfg_notioncore")
+dopath("statusd_laptopstatus")
+dopath("cfg_kludges")
+dopath("cfg_layouts")
+
+dopath("mod_query")
+dopath("mod_menu")
+dopath("mod_tiling")
+dopath("mod_statusbar")
+dopath("mod_sp")
+dopath("look_alex")
+--dopath("mod_xinerama")
---
+++ update-script.sh
@@ -0,0 +1,1 @@
+cp ~/.notion/* ~/Projects/settings/notion/
