84e56552ba4c6f8703bb2ca675eb411ab61c8f5c
John Smith <private@unknown.com>
2012-12-29 13:36:48 -0800

    added git settings file

--- update-script.sh
+++ update-script.sh
@@ -2,3 +2,4 @@ cp ~/.notion/* ~/Projects/settings/notion/
 cp ~/.bashrc ~/Projects/settings/home/
 cp ~/.bash_profile ~/Projects/settings/home/
 cp ~/.tmux.conf ~/Projects/settings/home/
+cp ~/.gitconfig ~/Projects/settings/home/
