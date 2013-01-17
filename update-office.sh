e1b60827ec3bee5ab3e9b8f93556f0882f4c1cd2
John Smith <private@unknown.com>
2013-01-17 15:23:49 -0800

    added code for dual monitors and created office script

---
+++ update-office.sh
@@ -0,0 +1,6 @@
+cp ~/.notion/* ~/projects/settings/notion/
+cp ~/.bashrc ~/projects/settings/home/
+cp ~/.bash_profile ~/projects/settings/home/
+cp ~/.tmux.conf ~/projects/settings/home/
+cp ~/.gitconfig ~/projects/settings/home/
+cp ~/.xprofile ~/projects/settings/dual_monitor_home/
